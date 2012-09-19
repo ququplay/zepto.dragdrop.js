7d91759ed50c729781a6498aa80dbcc658225a35
Michal Kuklis <michal.kuklis@gmail.com>
2012-09-19 03:57:21 -0400

    added missing comma

--- zepto.dragdrop.js
+++ zepto.dragdrop.js
@@ -1,12 +1,12 @@
 /**
- * zepto.dragddrop.js v0.1.0 - Drag & Drop for Zepto with touch and mouse events.
+ * zepto.dragdrop.js v0.1.0 - Drag & Drop for Zepto with touch and mouse events.
  *
  * Copyright 2012, Michal Kuklis
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
  **/
 
-(function ($) {
+;(function ($) {
 
   "use strict";
 
@@ -130,14 +130,14 @@
 })(Zepto);
 
 /**
- * zepto.dd.js v0.1.0 - Drag & Drop for Zepto with touch and mouse events.
+ * zepto.dragdrop.js v0.1.0 - Drag & Drop for Zepto with touch and mouse events.
  *
  * Copyright 2012, Michal Kuklis
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
  **/
 
-(function ($) {
+;(function ($) {
 
   "use strict";
 
@@ -294,7 +294,7 @@
   }
 })(Zepto);
 
-(function ($) {
+;(function ($) {
 
   "use strict";
 
